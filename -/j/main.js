1e484fd72bad2166fa3552e01d1f81c602eaae4f
Gregory Sarault <gregsarault@gmail.com>
2013-07-17 17:33:02 -0400

    easing for scroll feature

--- -/j/main.js
+++ -/j/main.js
@@ -31,7 +31,9 @@ $(document).ready(function () {
               var target = $(this.hash);
               if (target.length == 0) target = $('a[name="' + this.hash.substr(1) + '"]');
               if (target.length == 0) target = $('html');
-              $('html, body').animate({ scrollTop: target.offset().top }, 700);
+/*              $('html, body').animate({ scrollTop: target.offset().top }, 1000, "easeInOut" );*/
+               $('html, body').animate({ scrollTop: target.offset().top }, {duration: 800, easing:"easeInOutQuart"});
+              
               return false;
           });
           

d313c80bd76fa1e23b567f19b2d97c555bd7475c
Gregory Sarault <gregsarault@gmail.com>
2013-07-23 13:29:04 -0400

    adding twitter fallow

--- contact/?error/index.php
+++ contact/?error/index.php
@@ -216,8 +216,10 @@
             </div><!-- .copywright -->    
             
             <div class="social">
-                <p><a class="content-link" href="">&#64;carywood</a></p>
+                <p><a class="content-link" href="https://twitter.com/carywood" data-show-count="false">&#64;carywood</a></p>
+                <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
             </div><!-- .social -->
+            
             
         </section><!-- .footer -->    
         

b87742ffd463a51124198c47e0d848c9eb854311
Gregory Sarault <gregsarault@gmail.com>
2013-07-23 10:56:08 -0400

    adding error page

--- contact/script/index.php
+++ contact/script/index.php
@@ -24,7 +24,7 @@
     
     if (strtolower($contactTest) != '' || $contactName == '' || $contactEmail == '' || $contactMessage == ''  ) {
         //die('Wrong access code');
-        header( "Location: /contact/?error" );
+        header( "Location: /contact/%3ferror/#contact" );
         exit;
     }	
 	
@@ -47,7 +47,7 @@
 	........................................................................................*/
    
 	if(!isValidEmail($contactEmail)) {  
-		header( "Location: /contact/?error" );
+		header( "Location: /contact/%3ferror/#contact" );
 	}else {
 	 	mail($sendTo, $subject, $contactBody, $headers);
 		header( "Location: /contact/thanks/" );
@@ -63,26 +63,6 @@
 	function isValidEmail($email){
 	    return eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$", $email);
 	}
-	
-	
-	/* http://roshanbh.com.np/2007/12/getting-real-ip-address-in-php.html */ 
-	/*
-	function getRealIpAddr()
-	    {
-	        if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
-	        {
-	          $ip=$_SERVER['HTTP_CLIENT_IP'];
-	        }
-	        elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
-	        {
-	          $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
-	        }
-	        else
-	        {
-	          $ip=$_SERVER['REMOTE_ADDR'];
-	        }
-	        return $ip;
-	    }*/
 	
 
 ?>
